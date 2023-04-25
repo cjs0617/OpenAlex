3d1c9d4189747103332c3574eabe32c74f710de3
Jian Shi <jupyter-jianshi@andrew.cmu.edu>
2023-04-25 02:24:15 +0000

    create package

---
+++ pkg/setup.py
@@ -0,0 +1,15 @@
+"""A package for OpenAlex utilities."""
+
+from setuptools import setup
+
+setup(
+    name="s23",
+    version="0.0.1",
+    description="OpenAlex utilities",
+    maintainer="Jian Shi",
+    maintainer_email="jianshi@andrew.cmu.edu",
+    license="MIT",
+    packages=["s23"],
+    scripts=[],
+    long_description="""A set of OpenAlex utilities""",
+)
